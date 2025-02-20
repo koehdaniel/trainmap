722033175ef1013d148e357dc702b0459fccf027
Daniel Köhler <daniel.koehler@atruvia.de>
2025-02-21 00:53:53 +0100

    fetch route

---
+++ route.php
@@ -0,0 +1,8 @@
+<?php
+$url = "https://signal.eu.org/osm/eu/route/v1/train/13.398937,52.510885;12.482932,41.89332?overview=false&alternatives=true&steps=true";
+
+$route = json_decode(file_get_contens($url));
+
+print_r($route);
+?>
+
