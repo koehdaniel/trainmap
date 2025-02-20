1ce83f20b543da49f275112a4b1f3a9455afc4d4
Daniel Köhler <daniel.koehler@atruvia.de>
2025-02-21 00:59:09 +0100

    fetch route

--- route.php
+++ route.php
@@ -1,8 +1,16 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+
 $url = "https://signal.eu.org/osm/eu/route/v1/train/13.398937,52.510885;12.482932,41.89332?overview=false&alternatives=true&steps=true";
 
 $route = json_decode(file_get_contents($url));
 
-print_r($route);
+$steps = array();
+foreach($route->routes->legs->steps as $step){
+    $steps[] = $step->intersections->location;
+}
+
+print_r($steps);
 ?>
 
