2eb1969205244a99135b609255b2146c58489761
Daniel Köhler <daniel.koehler@atruvia.de>
2025-02-21 16:57:23 +0100

    fix execute

--- location.php
+++ location.php
@@ -10,6 +10,7 @@ function searchForName($table, $name, $suggestable=0, $limit=20){
     $sql = "SELECT id, name, longitude, latitude FROM `$table` WHERE `name` LIKE ? AND is_suggestable=$suggestable LIMIT $limit";
     $sql = $conn->prepare($sql);
     $sql->bind_param("s", $name);
+    $sql->execute();
     return $sql;
 }
 function addToArray(&$array, $dbResult){
@@ -34,17 +35,17 @@ if($search[0] == "*"){
 }
 
 $results = array();
-addToArray($results, searchForName("stations", $search)->execute()->get_results());
+addToArray($results, searchForName("stations", $search)->get_results());
 
 //Not enough results, try wildcard search if not yet done
 if(count($results) < 2 && $search[0] != "%"){
     $search = "%$search";
-    addToArray($results, searchForName("stations", $search)->execute()->get_results());
+    addToArray($results, searchForName("stations", $search)->get_results());
 }
 
 //Not enough results, try city search with wildcard since already enforced anyway
 if(count($results) < 2){
-    addToArray($results, searchForName("cities", $search)->execute()->get_results());
+    addToArray($results, searchForName("cities", $search)->get_results());
 }
 
 echo json_encode($results);
