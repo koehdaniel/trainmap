9ee42ef176188fccdbcb63b450fd765537e44017
Daniel Köhler <daniel.koehler@atruvia.de>
2025-02-21 02:11:41 +0100

    Include start and Ende

--- route.php
+++ route.php
@@ -2,17 +2,19 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
-$url = "https://signal.eu.org/osm/eu/route/v1/train/" . $_GET["from"] . ";" . $_GET["to"] . "?overview=false&alternatives=true&steps=true";
+$from = $_GET["from"];
+$to = $_GET["to"];
+$url = "https://signal.eu.org/osm/eu/route/v1/train/" . $from . ";" . $_GET["to"] . "?overview=false&alternatives=true&steps=true";
 
 $route = json_decode(file_get_contents($url));
 
 $steps = array();
-$steps[] = $_GET["from"];
+$steps[] = explode(",", $from);
 foreach($route->routes[0]->legs[0]->steps as $step){
     foreach($step->intersections as $intersection){
         $steps[] = $intersection->location;
     }
 }
-$steps[] = $_GET["to"];
+$steps[] = explode(",", $to);
 
 echo json_encode($steps);
