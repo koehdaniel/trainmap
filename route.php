0cd0f073cf3b87a376d7a8beb9c7dc81d47e1b0f
Daniel Köhler <daniel.koehler@atruvia.de>
2025-02-22 01:30:12 +0100

    swap latitude and longitude if wanted

--- route.php
+++ route.php
@@ -2,24 +2,27 @@
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 
-$from = $_GET["from"];
-$to = $_GET["to"];
-$url = "https://signal.eu.org/osm/eu/route/v1/train/" . $from . ";" . $_GET["to"] . "?overview=false&alternatives=true&steps=true";
+$fromLat = $_GET["from_lat"];
+$fromLon = $_GET["from_lon"];
+$from = $fromLat . "," . $fromLon;
+
+$toLat = $_GET["to_lat"];
+$toLon = $_GET["to_lon"];
+$to = $toLat . "," . $toLon;
+
+$latitudeFirst = $_GET["order"] == "lat,lon";
+
+$url = "https://signal.eu.org/osm/eu/route/v1/train/" . $from . ";" . $to . "?overview=false&alternatives=true&steps=true";
 
 $route = json_decode(file_get_contents($url));
 
 $steps = array();
-$steps[] = explode(",", $from);
+$steps[] = $latitudeFirst ? array($fromLat, $fromLon) : array($fromLon, $fromLat);
 foreach($route->routes[0]->legs[0]->steps as $step){
     foreach($step->intersections as $intersection){
-        if($_GET["order"] == "lat,lon"){
-            $steps[] = array($intersection->location[1], $intersection->location[0]);
-        }
-        else{
-            $steps[] = $intersection->location;
-        }
+        $steps[] = $latitudeFirst ? array($intersection->location[1], $intersection->location[0]) : $intersection->location;
     }
 }
-$steps[] = explode(",", $to);
+$steps[] = $latitudeFirst ? array($toLat, $toLon) : array($toLon, $toLat);
 
 echo json_encode($steps);
